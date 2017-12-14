36009473821c3f2d2fb7c0dfdc9d208f4bbfc885
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-12-14 17:17:55 +0530

    Remove setting up PL/pgSQL in migrations
    Avoids errors if db user has limited permission and postgresql enables plpgsql by default anyway.
    
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- migrations/1_setup.up.sql
+++ migrations/1_setup.up.sql
@@ -1,8 +1,3 @@
--- Enable plpgsql
-create extension if not exists plpgsql with schema pg_catalog;
-
-comment on extension plpgsql is 'PL/PGSQL procedural language';
-
 -- We use the `public` schema by default
 create schema if not exists public;
 
