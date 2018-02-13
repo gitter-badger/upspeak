5d5279298b74b251fe8355e29d1a1f366b9f5307
Kaustav Das Modak <kaustav@applait.com>
2018-02-14 01:18:21 +0530

    Add down migration for genesis
    Signed-off-by: Kaustav Das Modak <kaustav@applait.com>

--- migrations/1_genesis.down.sql
+++ migrations/1_genesis.down.sql
@@ -1,1 +1,13 @@
--- We'll be friendly and do nothing here
+drop table public.threads;
+drop table audit.node_revisions;
+drop table public.nodes;
+drop table public.node_data_types;
+drop table public.workspace_members;
+drop table public.workspaces;
+drop table public.namespaces;
+drop table public.team_members;
+drop table public.teams;
+drop table public.users;
+drop sequence public.global_id_seq;
+drop function public.generate_id(text, integer);
+drop function public.trigger_on_node_revision();
