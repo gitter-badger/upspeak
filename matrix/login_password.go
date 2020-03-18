314044548ea5d899929ca8985c5d2062bef8b43c
Kaustav Das Modak <kaustav.dasmodak@gmail.com>
2020-03-19 01:57:27 +0530

    Modify login password & add placeholder test file
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@gmail.com>

--- matrix/login_password.go
+++ matrix/login_password.go
@@ -1,51 +1,41 @@
 package matrix
 
-type UserIdentifier struct {
-	Type string `json:type`
-	User string `json:user`
+// loginPasswordRequest is the structure used to send a login password request
+type loginPasswordRequest struct {
+	Type       string         `json:"type"`
+	Identifier UserIdentifier `json:"identifier"`
+	Password   string         `json:"password"`
+	DeviceID   string         `json:"device_id"`
+	DeviceName string         `json:"initial_device_display_name"`
 }
 
-type ServerInformation struct {
-	BaseURL string `json:base_url`
-}
-
-type DiscoveryInformation struct {
-	HomeServer     ServerInformation `json:m.homeserver`
-	IdentityServer ServerInformation `json:m.identity_server`
-}
-
-type LoginPasswordRequest struct {
-	Type                     string         `json:type`
-	Identifier               UserIdentifier `json:identifier`
-	Password                 string         `json:password`
-	DeviceID                 string         `json:device_id`
-	InitialDeviceDisplayName string         `json:initial_device_display_name`
-}
-
+// LoginPasswordResponse is the structure of response for login password
+// requests
 type LoginPasswordResponse struct {
-	UserID      string               `json:user_id`
-	AccessToken string               `json:access_token`
-	DeviceID    string               `json:device_id`
-	WellKnown   DiscoveryInformation `json:well_known,omitempty`
+	UserID      string               `json:"user_id"`
+	AccessToken string               `json:"access_token"`
+	DeviceID    string               `json:"device_id"`
+	WellKnown   DiscoveryInformation `json:"well_known,omitempty"`
 }
 
+// LoginPassword sends a login request to Matrix with a password
 func LoginPassword(homeserverURL, userID, password string) (LoginPasswordResponse, error) {
 	var res LoginPasswordResponse
 	c, err := newClient(homeserverURL, userID)
 	if err != nil {
 		return res, err
 	}
-	req := LoginPasswordRequest{
+	req := loginPasswordRequest{
 		Type: "m.login.password",
 		Identifier: UserIdentifier{
 			Type: "m.id.user",
 			User: userID,
 		},
-		Password:                 password,
-		InitialDeviceDisplayName: "Upspeak",
+		Password:   password,
+		DeviceName: "Upspeak",
 	}
 	if err = c.send("POST", "login", nil, req, res); err != nil {
 		return res, err
 	}
-	return res, err
+	return res, nil
 }
