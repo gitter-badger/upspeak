17319aa1fcedc14d041518d7a88f952970391763
Kaustav Das Modak <kaustav.dasmodak@gmail.com>
2020-04-05 01:21:30 +0530

    Attempt to create upspeak/node as a model
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@gmail.com>

--- upspeak/node.go
+++ upspeak/node.go
@@ -14,8 +14,35 @@ type Node struct {
 	Type    string    `json:"type"`
 	Sender  matrix.ID `json:"sender"`
 
-	// Relations
-	PreviousNode *Node // Relation to the node before this one
-	SourceNode   *Node // The origin of the thread
-	Room         *Room // The room this Node belongs to
+	// Graph
+	PreviousNode  *Node   // Relation to the node before this one
+	SourceNode    *Node   // The origin of the thread
+	AdjacentNodes []*Node // Nodes that have been forked from this node
+
+	// Part of
+	Room *Room // The room this Node belongs to
+}
+
+// NewNode creates a new Node instance. It does not persist the created Node.
+// Call Node.Save() on the returned Node to persist it.
+func NewNode(id string, content Content) Node {
+	return Node{
+		ID:      id,
+		Content: content,
+	}
+}
+
+// Save persists the Node in DB
+func (n *Node) Save() {
+
+}
+
+// FindNode searches a Node by ID
+func FindNode(id string) (*Node, error) {
+	var n Node
+	var err error
+
+	// TODO: Add logic
+
+	return &n, err
 }
